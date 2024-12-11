dbc172a0fb483e5d1dd874a88c01b6676cc3a1bd
A00488600 <sai.vishnu.teja.reddy.pulim@smu.ca>
2024-12-11 13:11:22 -0400

    Retrieve all posts

--- GameOnV2.Server/Controllers/PostController.cs
+++ GameOnV2.Server/Controllers/PostController.cs
@@ -15,83 +15,41 @@ public class PostsController : ControllerBase
 
     // GET: api/posts
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Post>>> GetPosts()
+    public async Task<ActionResult<IEnumerable<object>>> GetPosts()
     {
-        return await _context.Posts.ToListAsync();
-    }
-
-    // GET: api/posts/5
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Post>> GetPost(int id)
-    {
-        var post = await _context.Posts.FindAsync(id);
-
-        if (post == null)
-        {
-            return NotFound();
-        }
-
-        return post;
-    }
-
-    // POST: api/posts
-    [HttpPost]
-    public async Task<ActionResult<Post>> PostPost(Post post)
-    {
-        _context.Posts.Add(post);
-        await _context.SaveChangesAsync();
-
-        return CreatedAtAction("GetPost", new { id = post.Id }, post);
-    }
-
-    // PUT: api/posts/5
-    [HttpPut("{id}")]
-    public async Task<IActionResult> PutPost(int id, Post post)
-    {
-        if (id != post.Id)
-        {
-            return BadRequest();
-        }
-
-        _context.Entry(post).State = EntityState.Modified;
-
-        try
-        {
-            await _context.SaveChangesAsync();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-            if (!PostExists(id))
+        var postsWithUsernames = await _context.Posts
+            .Include(p => p.User)
+            .Select(p => new
             {
-                return NotFound();
-            }
-            else
-            {
-                throw;
-            }
-        }
+                p.Id,
+                p.Description,
+                p.Location,
+                p.FromTime,
+                p.TillTime,
+                p.Date,
+                p.SportName,
+                p.UserID,
+                Username = p.User.Username
+            })
+            .ToListAsync();
 
-        return NoContent();
+        return Ok(postsWithUsernames);
     }
 
-    // DELETE: api/posts/5
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeletePost(int id)
-    {
-        var post = await _context.Posts.FindAsync(id);
-        if (post == null)
-        {
-            return NotFound();
-        }
+    
+}
 
-        _context.Posts.Remove(post);
-        await _context.SaveChangesAsync();
+public class UserRequest
+{
+    public int UserId { get; set; }
+}
 
-        return NoContent();
-    }
-
-    private bool PostExists(int id)
-    {
-        return _context.Posts.Any(e => e.Id == id);
-    }
+public class UpdatePostRequest
+{
+    public int PostID { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public DateTime FromTime { get; set; }
+    public DateTime TillTime { get; set; }
+    public string SportName { get; set; }
 }
